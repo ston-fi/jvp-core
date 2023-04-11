138101422b531d7337f72b92879e65b9d786eb7d
sikvelsigma <63592183+sikvelsigma@users.noreply.github.com>
2023-04-11 16:32:13 +0300

    fix script commented out

--- scripts/deployRegister.ts
+++ scripts/deployRegister.ts
@@ -11,30 +11,21 @@ import { readConfig, updateConfig } from '../helpers/helpers';
 export async function run(provider: NetworkProvider) {
     let config = readConfig()
 
-    // const senderAddress = provider.sender().address as Address
+    const senderAddress = provider.sender().address as Address
     
-    // const register = provider.open(RegisterContract.createFromConfig({
-    //     adminAddress: senderAddress,
-    //     adminPendingAddress: null,
-    //     addressDict: Dictionary.empty(Dictionary.Keys.BigUint(256), Dictionary.Values.Cell()),
-    //     voteStorageCode: await compile('VoteStorage'),
-    //     voteStatusCode: await compile('VoteStatus'),
-    // }, await compile('Register')));
+    const register = provider.open(RegisterContract.createFromConfig({
+        adminAddress: senderAddress,
+        adminPendingAddress: null,
+        addressDict: Dictionary.empty(Dictionary.Keys.BigUint(256), Dictionary.Values.Cell()),
+        voteStorageCode: await compile('VoteStorage'),
+        voteStatusCode: await compile('VoteStatus'),
+    }, await compile('Register')));
 
-    // await register.sendDeploy(provider.sender(), toNano('0.05'));
+    await register.sendDeploy(provider.sender(), toNano('0.05'));
 
-    // await provider.waitForDeploy(register.address);
+    await provider.waitForDeploy(register.address);
 
-    // config.registerAddress = register.address
-    config.addVotersList = [
-        Address.parseFriendly("EQAG1Jse61i_IBRILwaHXT9CG6vv1kF-x4rqWZ0GK8eEkMW7").address,
-        Address.parseFriendly("EQAO9JsDEbOjnb8AZRyxNHiODjVeAvgR2n03T0utYgkpx79x").address
-    ]
-    config.removeVoterList = [
-        Address.parseFriendly("EQAG1Jse61i_IBRILwaHXT9CG6vv1kF-x4rqWZ0GK8eEkMW7").address,
-        Address.parseFriendly("EQAO9JsDEbOjnb8AZRyxNHiODjVeAvgR2n03T0utYgkpx79x").address
-    ]
-
+    config.registerAddress = register.address
     
     updateConfig(config)
 }
